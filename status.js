7bfc0562eb37cd95eb708e55443bc7ffded60d47
Naufal Kurniawan <cvtataureskin078@gmail.com>
2026-02-23 20:14:02 +0700

    status.js

---
+++ status.js
@@ -0,0 +1,6 @@
+// status.js
+var maintenanceConfig = {
+    isMaintenance: false, // Ubah ke true untuk mengunci web
+    message: "Kami sedang melakukan pembaruan sistem untuk kenyamanan Anda.",
+    estimatedFinish: "24 Februari 2026, 10:00 WIB"
+};
