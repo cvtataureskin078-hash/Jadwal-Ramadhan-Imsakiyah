87a767c97ca40e247e5f049fc53768c4323ceb0d
Naufal Kurniawan <cvtataureskin078@gmail.com>
2026-02-25 22:22:52 +0700

    status.js

--- status.js
+++ status.js
@@ -4,6 +4,6 @@ var maintenanceConfig = {
     maintenanceMessage: "Kami sedang melakukan pembaruan rutin aplikasi untuk meningkatkan kecepatan akses jadwal. Mohon tunggu ya!",
     maintenanceETA: "Aplikasi akan kembali aktif", 
     
-    isClosed: false,      
+    isClosed: true,      
     closedMessage: "Aplikasi Jadwal Imsakiyah Ramadan 1447 H telah resmi ditutup. Terima kasih, sampai jumpa di Ramadan tahun depan!",
 };
